530dee8fef87199d17c9100a9cdd2067fde61109
unknown <srujanmahaja5@gmail.com>
2021-10-10 10:13:56 +0530

    Adding new Conditon

--- SnakeAndladder.cs
+++ SnakeAndladder.cs
@@ -1,40 +1,66 @@
 ﻿using System;
 
-<<<<<<< HEAD
-namespace UC_2_diceRoll
+
+namespace UC_4_repeatTillPosition100
 {
-    class UC_2_diceRoll
-=======
-namespace UC_3_optionsCheck
-{
-    class UC_3_optionsCheck
->>>>>>> UC_3checkoptions
+    class UC_4_repeatTillPosition100
     {
+        public const int maxPosition = 100;
+        public const int samePosition = 0;
+        public const int forward = 1;
+        public const int backward = 2;
+        public static void repeatTillPosition100()
+        {
+            Console.WriteLine("-------------------------------------");             //For separation
+
+            //Variables
+            int currentPosition = 1;
+
+            //Computation
+            while (currentPosition <= maxPosition)
+            {
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                Console.WriteLine("You got " + dice + " in dice roll.");
+
+                Random random1 = new Random();
+                int options = random1.Next(0, 3);
+
+                if (options == samePosition)
+                {
+                    Console.WriteLine("You Got No Option Stay in the Same Place.");
+                }
+                else if (options == forward)
+                {
+                    currentPosition = currentPosition + dice;
+                    Console.WriteLine("You Got Ladder Your Moving Forward by " + dice + " numbers.");
+                }
+                else if (options == backward)
+                {
+                    currentPosition = currentPosition - dice;
+                    Console.WriteLine("You Got Snakebite Your Moving Backward by " + dice + " numbers.");
+                }
+                if (currentPosition >= 100)
+                {
+                    Console.WriteLine("Congratulations You Won the Game Your at 100 position :)");
+                    break;
+                }
+                else if (currentPosition <= 0)
+                {
+                    Console.WriteLine("You lost the Game Your at 0 position. Restarting the Game :(");
+                    currentPosition = currentPosition - currentPosition;
+                }
+                Console.WriteLine("Your current Position is: " + currentPosition);
+                Console.WriteLine("-------------------------------------");                 //For separation
+            }
+        }
         static void Main(string[] args)
         {
-            Random random = new Random();
-<<<<<<< HEAD
-            int diceRoll = random.Next(0, 7);
-            Console.WriteLine("You got " + diceRoll + " in dice roll.");
-=======
-            int diceRoll = random.Next(1, 6);
-            Console.WriteLine("You got " + diceRoll);
-            //Constants
-            const int samePosition = 0;
-            const int forward = 1;
-            Random random1 = new Random();
-            int options = random1.Next(0, 2);
-            if (options == samePosition)
-            {
-                Console.WriteLine("You Got No Option Stay in the Same Place.");
-            }
-            else if (options == forward)
-            {
-                Console.WriteLine("You Got Ladder Your moving ahead by " + diceRoll + " numbers.");
-            }
-            else
-                Console.WriteLine("You Got Snake Your Moving Backward by " + diceRoll + " numbers.");
->>>>>>> UC_3checkoptions
-        }
+            Console.WriteLine("HELLO WELCOME TO SNAKE AND LADDER GAME :)");
+            Console.WriteLine("Single Player Game is started Your at position 0.");
+            UC_4_repeatTillPosition100.repeatTillPosition100();
+            Console.ReadLine();
     }
+        }
+    
 }
