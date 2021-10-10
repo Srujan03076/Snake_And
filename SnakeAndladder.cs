ec8407d8d93286b401ad50de75d384b141d66a19
unknown <srujanmahaja5@gmail.com>
2021-10-10 10:38:21 +0530

    Adding new Conditon

--- SnakeAndladder.cs
+++ SnakeAndladder.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 
-namespace UC_5_exactWinningPosition100
+namespace UC_6_numberOfTimeDiceRolled
 {
-    class UC_5_exactWinningPosition100
+    class UC_6_numberOfTimeDiceRolled
     {
+
         public const int maxPosition = 100;
         public const int samePosition = 0;
         public const int forward = 1;
         public const int backward = 2;
-        public static void exactWinningPosition100()
+        public static void numberOfTimeDiceRolled()
         {
             Console.WriteLine("-------------------------------------");             //For separation
 
             //Variables
             int currentPosition = 1;
-            int newPosition = 1;
+            int newPosition = 0;
+            int numOfDiceRoll = 0;
 
             //Computation
             while (currentPosition <= maxPosition)
@@ -27,14 +29,16 @@ namespace UC_5_exactWinningPosition100
                 int options = random1.Next(0, 3);
 
                 newPosition = currentPosition + dice;
+                numOfDiceRoll++;
 
-                if (newPosition == 100)
+
+                if (newPosition == maxPosition)
                 {
-                    currentPosition = currentPosition - dice;
                     Console.WriteLine("Congratulations You Won the Game Your at 100 position :)");
+                    Console.WriteLine("Dice was rolled " + numOfDiceRoll + " times.");
                     break;
                 }
-                else if (newPosition > 100)
+                else if (newPosition > maxPosition)
                 {
                     Console.WriteLine("You Got No Option stay in same Position.");
                     Console.WriteLine("Your current Position is: " + currentPosition);
@@ -43,28 +47,34 @@ namespace UC_5_exactWinningPosition100
                 }
 
 
+
                 if (options == samePosition)
                 {
                     Console.WriteLine("You Got No Option Stay in the Same Place.");
                 }
                 else if (options == forward)
                 {
-                    currentPosition = currentPosition + dice;
+                    currentPosition += dice;
                     Console.WriteLine("You Got Ladder Your Moving Forward by " + dice + " numbers.");
                 }
                 else if (options == backward)
                 {
-                    currentPosition = currentPosition - dice;
+                    currentPosition -= dice;
                     Console.WriteLine("You Got Snakebite Your Moving Backward by " + dice + " numbers.");
                 }
-
-
-                if (currentPosition <= 0)
+                if (currentPosition >=0)
+                {
+                    Console.WriteLine("You Won the Game")
+                        break;
+                }
+                else if (currentPosition <= 0)
                 {
                     Console.WriteLine("You lost the Game Your at 0 position. Restarting the Game :(");
                     currentPosition = currentPosition - currentPosition;
                 }
+
                 Console.WriteLine("Your current Position is: " + currentPosition);
+                Console.WriteLine("Dice was rolled " + numOfDiceRoll + " times.");
                 Console.WriteLine("-------------------------------------");                 //For separation
             }
         }
@@ -72,9 +82,8 @@ namespace UC_5_exactWinningPosition100
         {
             Console.WriteLine("HELLO WELCOME TO SNAKE AND LADDER GAME :)");
             Console.WriteLine("Single Player Game is started Your at position 0.");
-            UC_5_exactWinningPosition100.exactWinningPosition100();
+            UC_6_numberOfTimeDiceRolled.numberOfTimeDiceRolled();
             Console.ReadKey();
         }
     }
 }
-    
